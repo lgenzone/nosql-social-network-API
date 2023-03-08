87059fa1cd653107bb9d0c1eb343096a9d9e55e5
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-03-08 14:44:01 -0800

    server running, created User model

---
+++ models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const userSchema = new Schema({
+    username:
+
+});
+
+module.exports = User; 
--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 const express = require('express');
 const db = require('./config/connection');
 const app = express();
-const routes = require('./routes');
+//const routes = require('./routes');
 
 const PORT = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
