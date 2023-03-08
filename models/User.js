599da99cecdab7166f4c69560e6f2d5fd04388f4
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-03-08 15:00:17 -0800

    updated User model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,34 @@
 const mongoose = require('mongoose');
 
 const userSchema = new Schema({
-    username:
-
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match:  [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Please enter a valid email address']
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    }],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
 });
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
 
 module.exports = User; 
