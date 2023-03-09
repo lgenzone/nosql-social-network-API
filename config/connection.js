b968d2c143ae8a3675f65ed9ed338f1735eaf586
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-03-09 15:34:42 -0800

    update URI

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 
-
-mongoose.connect('mongodb://localhost:27017/social_db', {
+mongoose.connect('mongodb://localhost/socialNetworkDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
