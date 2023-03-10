f79846e6ba30c50681fdec786d32a8f30378ea4b
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-03-09 16:55:38 -0800

    created thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+
+
+//GET, POST, PUT, DELETE /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought)
+
+//GET, PUT, DELETE /api/thoughts/:thoughtId
+router 
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+//POST and DELETE /api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(createReaction)
+    .delete(removeReaction);
